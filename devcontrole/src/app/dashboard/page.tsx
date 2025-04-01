c405430a52e952e8a530c6761d874f8543b8abcb
veigarj <veiga.cg@gmail.com>
2025-04-01 20:41:19 -0300

    feat: add dashboard and initial home

---
+++ devcontrole/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Container from '@/components/container'
+
+export default function Dashboard() {
+  return (
+    <Container>
+        <h1>Pagina dashboard</h1>
+    </Container>
+  )
+}
--- devcontrole/src/app/page.tsx
+++ devcontrole/src/app/page.tsx
@@ -1,9 +1,17 @@
-
+import imgHero from '@/assets/hero.svg'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="">
-
-    </div>
+    <main className='flex items-center flex-col justify-center min-h-[calc(100vh-80px)]'>
+      <h2 className='font-medium text-2xl mb-2'>Gerencie sua empresa</h2>
+      <h1 className='font-bold text-3xl mb-8 text-blue-600 md:text-4xl'>Atendimentos, clientes</h1>
+      <Image 
+      src={imgHero}
+      alt='Imagem hero do DevControle'
+      width={600}
+      className='max-w-sm md:max-w-xl'
+      />
+    </main>
   );
 }
