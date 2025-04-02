fc6ceff1b2ce6b336649d68c7babb482e3c91c1f
veigarj <veiga.cg@gmail.com>
2025-04-02 08:49:43 -0300

    feat: config initial NextAuth

--- devcontrole/src/app/layout.tsx
+++ devcontrole/src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import Header from "@/components/header";
+import { AuthProvider } from '@/providers/auth'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dev Controle - Seu sistema de gerenciamento.",
   description: "Dev Controle é um sistema de gerenciamento de projetos e tarefas.",
@@ -26,10 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
-        {children}
+        className={inter.className}>
+          <AuthProvider>
+            <Header />
+            {children}
+          </AuthProvider>
       </body>
     </html>
   );
