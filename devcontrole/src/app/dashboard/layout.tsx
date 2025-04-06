984de64cfac8189903065dacb7455c46ad4d5a4e
veigarj <veiga.cg@gmail.com>
2025-04-06 18:18:14 -0300

    feat: initial page dashboard

---
+++ devcontrole/src/app/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+import DashboardHeader from '@/app/dashboard/components/header'
+export default function DashboardLayout( { children }: {children: React.ReactNode} ) {
+  return (
+    <>
+    < DashboardHeader />
+    {children}
+    </>
+  )
+}
