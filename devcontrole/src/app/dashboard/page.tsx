8aec9dd221d24358742cb89cd07113b2bbd24255
veigarj <veiga.cg@gmail.com>
2025-04-06 19:07:15 -0300

    feat: initial config table for custumer dashboard page

--- devcontrole/src/app/dashboard/page.tsx
+++ devcontrole/src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import Container from '@/components/container'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { redirect } from 'next/navigation'
+import Link from 'next/link'
+import { TicketItem } from '@/app/dashboard/components/ticket/index'
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions)
@@ -12,7 +14,30 @@ export default async function Dashboard() {
 
   return (
     <Container>
-        <h1>Pagina dashboard</h1>
+      <main className='mt-9 mb-2'>
+        <div className='flex items-center justify-between '>
+        <h1 className="text-3xl font-bold">Chamados</h1>
+        <Link href="/dashboard/new" className="bg-blue-500 text-white px-4 py-1 rounded">
+          Abrir Chamado
+        </Link>
+        </div>
+
+        <table className='min-w-full my-2'>
+          <thead>
+            <tr>
+              <th className='font-medium text-left pl-1'>CLIENTE</th>
+              <th className='font-medium text-left hidden sm:block'>DATA CADASTRO</th>
+              <th className='font-medium text-left'>STATUS</th>
+              <th className='font-medium text-left'>#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TicketItem />
+            <TicketItem />
+
+          </tbody>
+        </table>
+      </main>
     </Container>
   )
 }
