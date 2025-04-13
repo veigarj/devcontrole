e636dda1b529e3d3f67a97cb9eb811c9b013ed3b
veigarj <veiga.cg@gmail.com>
2025-04-13 12:30:44 -0300

    feat: config form get customer and botton submit

--- devcontrole/src/app/dashboard/new/page.tsx
+++ devcontrole/src/app/dashboard/new/page.tsx
@@ -1,7 +1,27 @@
 import Container from "@/components/container";
 import Link from "next/link";
 
-export default function NewTicket() {
+// route autorization
+import { getServerSession   } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import prismaClient from "@/lib/prisma";
+
+export default async function NewTicket() {
+    const session = await getServerSession(authOptions)
+
+    // verify if user is logged
+    if(!session || !session.user){
+        redirect('/')
+    }
+
+    // get all customers is user logged
+    const customers = await prismaClient.custumer.findMany({
+        where: {
+            userId: session.user.id
+        }
+    })
+
   return (
     <Container>
         <main className='mt-9 mb-2'>
@@ -27,12 +47,32 @@ export default function NewTicket() {
                 required
             ></textarea>
 
-            <label className="mb-1 font-medium text-lg">Selecione o cliente</label>
-            <select
-                className='w-full border-gray-400 border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white'
+            {customers.length !== 0 && (
+                <>
+                    <label className="mb-1 font-medium text-lg">Selecione o cliente</label>
+                    <select
+                    className='w-full border-gray-400 border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white'
+                    >
+                    {customers.map( customer => (
+                        <option key={customer.id}value={customer.id}>{customer.name}</option>
+                    ))}
+                    </select>
+                </>
+            )}
+
+            {customers.length === 0 && (
+                <Link href='/dashboard/new/customer'>
+                    Você não tem nenhum cliente cadastrado, <span className="text-blue-500 font-medium">clique aqui para cadastrar</span>
+                </Link>
+            )}
+
+            <button 
+            type='submit'
+            className='bg-blue-500 text-white font-bold px-2 h-11 rounded-md my-4 disabled:bg-gray-400 disabled:cursor-not-allowed'
+            disabled={customers.length ===0}
             >
-                <option value='cliente1'>Cliente 1</option>
-            </select>
+                Cadastrar
+            </button>
         </form>
         </main>
     </Container>
